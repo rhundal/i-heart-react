0d34c49f960c4e6a6e4df4479303e309b460145a
Rajdeep Hundal <hrajdeep712@gmail.com>
2023-01-01 21:20:21 -0600

    i-heart-react-app

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+const headerMessage = 'I ❤️ React';
+
+const Header = () => {
+
+    return (
+
+        <div className='App-header'>
+
+            {headerMessage}
+
+        </div>
+    );
+
+};
+
+export default Header;
---
+++ src/components/HeartsList.js
@@ -0,0 +1,53 @@
+import CandyColoredHeart from "./CandyColoredHeart";
+//import Heart from "./Heart";
+
+const messages = [
+    'cool cud',
+    'me my <3',
+    'you are bear',
+    'team bear',
+    'time hug',
+    'fang',
+    'bog love',
+    'me have love',
+    'all hover',
+    'sweat poo',
+    'u hack',
+    'stank love',
+    'heart me',
+    'wink bear',
+    'bear bear',
+    'be my bear',
+    'yank o way',
+    'mage love',
+    'oy',
+    'in a fan'
+];
+
+const HeartsList = () => {
+
+    return (
+        <div className='hearts-container'>
+            {messages.map((message, index) => (
+                <CandyColoredHeart key={index} msg={message} />
+            ))}
+        </div>
+
+    );
+};
+
+/*
+const HeartsList = () => {
+
+    return (
+        <div className='hearts-container'>
+            {messages.map((message, index) => (
+                <Heart key={index} msg={message} />
+            ))}
+        </div>
+
+    );
+};
+*/
+
+export default HeartsList;
